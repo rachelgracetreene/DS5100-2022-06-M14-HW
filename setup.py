52529a261579f6a6dd84cbc3241023251ae48718
rachelegrace <94571546+rachelegrace@users.noreply.github.com>
2022-07-11 11:30:05 -0400

    Add files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='M14',
+      version='0.1',
+      description='A M14 homework package',
+      url='http://github.com/storborg/m14-demo',
+      author='R.C. Alvarado',
+      author_email='ontoligent@gmail.com',
+      license='MIT',
+      packages=['demo'])
